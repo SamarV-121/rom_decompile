c1fafceccadb8a4019f69c9628c260f46e57bbfb
ѕαмαя νιѕρυтє <samarvispute121@pm.me>
2022-05-04 19:42:54 +0530

    Update jadx.sh

--- jadx.sh
+++ jadx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-axel -n 10 -o rom.zip https://ota-manual-eu.allawnofs.com/ota/21/08/19/d7c859e7-0a63-410d-9e14-db0eb31a28ea.ozip
+axel -n 10 -o rom.zip https://gauss-componentotacostmanual-sg.allawnofs.com/remove-5001ab88e4c953041059ba0658d3ca1c/component-ota/22/04/12/58cbade4ebdc4531b9c16d1c9cb283ca.zip
 
 ./tools/rom.sh rom.zip out
 
